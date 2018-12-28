9ae4542eb8c7d8650095f676cbf89ab1ee5b3459
mperkins <mperkins@redhat.com>
2018-12-28 15:55:11 -0500

    working on templatized sketch

--- 000.js
+++ 000.js
@@ -3,7 +3,8 @@ import {lerp} from 'canvas-sketch-util/math';
 import * as  random from 'canvas-sketch-util/random';
 import {grid2d2} from "./lib/grid";
 import {TWO_PI} from "./lib/number";
-import {css, injectGlobal} from 'emotion';
+import {injectGlobal} from 'emotion';
+import {filledCircle} from "./lib/shapes";
 
 injectGlobal`
   body {
@@ -19,25 +20,38 @@ console.log(`Using seed ${random.getSeed()}`);
 
 const settings = {
   dimensions: [2048, 2048],
+  animate   : true,
+  fps       : 24,
+  duration  : 5,
   suffix    : `seed-${random.getSeed()}`
 };
 
-const grid = grid2d2(50).filter(() => random.value() > 0.5);
+const grid   = grid2d2(10); //.filter(() => random.value() > 0.5);
 const margin = 200;
 
 const sketch = () => {
-  return ({context, width, height}) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
-    grid.forEach(({radius, position, color, rotation}) => {
-      const [u, v] = position;
-      const x = lerp(margin, width - margin, u);
-      const y = lerp(margin, height - margin, v);
-      context.beginPath();
-      context.arc(x, y, radius * width, 0, TWO_PI, false);
-      context.fillStyle = color;
-      context.fill();
-    })
+
+  // Static template
+  // return ({context, width, height}) => {};
+
+  // Animated template
+  // return {render({context, width, height, time, playhead}) {}};
+
+  return {
+    render({context, width, height, time, playhead}) {
+      context.fillStyle = 'white';
+      context.fillRect(0, 0, width, height);
+      grid.forEach(({position, color}) => {
+        const [u, v] = position;
+        const x      = lerp(margin, width - margin, u);
+        const y      = lerp(margin, height - margin, v);
+        const t = Math.abs(Math.sin(playhead * (Math.PI * 2))) * .1;
+
+        const radius = Math.abs(random.noise3D(u, v, time)*3) * t;
+
+        filledCircle({context, x, y, radius: radius * width, color});
+      })
+    }
   };
 };
 
